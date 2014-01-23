4a32623263a225fec87a17282c081ba675c59649
Christian Fei <info@christian-fei.com>
2014-01-23 20:01:39 +0100

    testing falsy test

--- spec/phonebookSpec.js
+++ spec/phonebookSpec.js
@@ -4,11 +4,11 @@ describe('phonebook', function(){
 	/**/
 	it('should connect to the database', function(done){
 		phonebook.connect( process.env.MONGO_URL, 'contacts', function(success){      
-			expect(success).toBeTruthy();
+			expect(success).toBeFalsy();
 			done();
 		});
 	},3000);
-	it('should result be an existing number', function(done){
+	it('should result in an existing number', function(done){
 		phonebook.exists(123, function(err,doc){
 			expect(doc).toBeTruthy();
 			done();
