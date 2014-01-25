4673cd29c5a83b7db575cf54da6c103b0b9f77a1
Christian Fei <info@christian-fei.com>
2014-01-25 11:37:44 +0100

    search results with template, restore old list if query is not valid

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,20 @@
 var searchToggle = document.querySelector('.search-toggle');
 var searchInput = document.querySelector('.search-input');
 
+/*
+  to restore the original list if the search query is not valid
+*/
+var entries = document.querySelector('.entries');
+var origList = entries ? entries.innerHTML : '';
+
+/* template of search results */
+var searchResultTemplate = '<li><span class="name">{name}</span><span class="surname">{surname}</span><span class="number">{number}</span><a href="/edit/{id}" class="edit">edit</a><a href="/delete/{id}" class="delete">delete</a></li>'
+
+/*
+  the XMLHttpRequest to retrieve search results from the server
+*/
 var xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+
 /*
   the search toggle only exists on the home page
 */
@@ -12,22 +25,38 @@ function toggleSearch(e){
 function search(){
   var query = searchInput.value;
   query = query.trim();
-  query = encodeURIComponent(query);
 
-  //abort previous request
-  xhr.abort();
-  xhr.open('GET', '/search/' + query, true);
-  xhr.onreadystatechange = function(){
-    if( xhr.status == 200 && xhr.readyState == 4 ){
-      try{
-        var results = JSON.parse( xhr.responseText );
-        console.log( results );
-      }catch(e){
-        console.log( e );
+  if( query ){
+    query = encodeURIComponent(query);
+
+    //abort previous request
+    xhr.abort();
+    xhr.open('GET', '/search/' + query, true);
+    xhr.onreadystatechange = function(){
+      if( xhr.status == 200 && xhr.readyState == 4 ){
+        try{
+          var results = JSON.parse( xhr.responseText );
+          entries.innerHTML = '';
+          console.log( results );
+          renderSearchResults( results );
+        }catch(e){
+          console.log( e );
+        }
       }
     }
+    xhr.send();
+  }else{
+    entries.innerHTML = origList;
   }
-  xhr.send();
+}
+function renderSearchResults(results){
+  results.forEach(function(entry){
+    output = searchResultTemplate;
+    output = searchResultTemplate.replace(/\{(.*?)\}/g, function(match, property) {
+        return entry[property];
+    });
+    entries.innerHTML += output;
+  });
 }
 if(searchToggle && searchInput){
   searchToggle.addEventListener('click', toggleSearch, true);
